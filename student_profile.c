2b34cef02bbb39489144618ba90bb9de460525b0
Erliss17 <em256@bgt.school>
2026-04-14 10:36:16 +0200

    Add student profile display in student_profile.c

--- student_profile.c
+++ student_profile.c
@@ -1,1 +1,10 @@
+#include <stdio.h>
 
+int main() {
+    printf("Student Profile\n");
+    printf("Name: Erlis Maxhuni\n");
+    printf("Age: 17\n");
+    printf("University: BGT\n");
+
+    return 0;
+}
