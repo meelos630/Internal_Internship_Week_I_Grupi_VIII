68ed7fb7da094d6e741634f9d3cab394d9d1864c
meelos630 <ml275@bgt.school>
2026-04-14 10:29:31 +0200

    Add hello_world.c to print 'Hello, World!'

---
+++ Day1/hello_world.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Main function. Program starts here.
+int main() {
+    // Print message to the screen
+    printf("Hello, World!\n");
+
+    // End program successfully
+    return 0;
+}
