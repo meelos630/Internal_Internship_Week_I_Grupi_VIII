15d1a1d4d6e65b39bcf544beb74c9751efbaa4c4
JorikSopjani <js266@bgt.school>
2026-04-14 10:37:32 +0200

    Create Student_profile.c

---
+++ Day1/Student_profile.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // Deklarimi i variablave me te dhenat tua
+    char emri[] = "Jorik";
+    int mosha = 16;
+
+    // Printimi i profilit te studentit
+    printf("Profili i Studentit:\n");
+    printf("-------------------\n");
+    printf("Emri: %s\n", emri);
+    printf("Mosha: %d\n", mosha);
+    printf("Shkolla: British Gymnasium of Technology\n");
+
+    return 0;
+}
