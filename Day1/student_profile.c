660c05799da918e59776f732c1a064edd31a3096
meelos630 <ml275@bgt.school>
2026-04-14 10:37:12 +0200

    Add student_profile.c to display student information

---
+++ Day1/student_profile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>  // Allows us to use printf()
+
+int main() {        // Program starts here
+
+    // Print name
+    printf("Name: Melos\n");
+
+    // Print age
+    printf("Age: 15\n");
+
+    // Print role
+    printf("Role: Student\n");
+
+    return 0;       // Ends the program successfully
+}
