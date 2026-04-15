ae755e8539be96cf035c80381692d70d31b185be
Imer-w <im263@bgt.school>
2026-04-15 11:24:17 +0200

    Task2 completed - grading system using if else

--- Day2/task2.c
+++ Day2/task2.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+
+    int score = 78;  
+
+    if (score < 0 || score > 100) {
+        printf("Invalid input!\n");
+    }
+    else if (score >= 90) {
+        printf("Shkelqyeshem\n");
+    }
+    else if (score >= 75) {
+        printf("Shume mire\n");
+    }
+    else if (score >= 50) {
+        printf("Mire\n");
+    }
+    else {
+        printf("Ne permiresim\n");
+    }
+
+    return 0;
+}
