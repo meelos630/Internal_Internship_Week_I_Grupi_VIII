a4650263d57db5f77a3d3e921390af8b84640e4c
Erliss17 <em256@bgt.school>
2026-04-15 13:26:36 +0200

    Add day of the week switch case program

---
+++ Day2/day2_task3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int day;
+
+    // Marrja e input-it
+    printf("Enter a number (1-7): ");
+    scanf("%d", &day);
+
+    // Switch për ditët e javës
+    switch(day) {
+        case 1:
+            printf("Monday - Workday\n");
+            break;
+        case 2:
+            printf("Tuesday - Workday\n");
+            break;
+        case 3:
+            printf("Wednesday - Workday\n");
+            break;
+        case 4:
+            printf("Thursday - Workday\n");
+            break;
+        case 5:
+            printf("Friday - Workday\n");
+            break;
+        case 6:
+            printf("Saturday - Weekend\n");
+            break;
+        case 7:
+            printf("Sunday - Weekend\n");
+            break;
+        default:
+            printf("Invalid input! Please enter a number from 1 to 7.\n");
+    }
+
+    return 0;
+}
