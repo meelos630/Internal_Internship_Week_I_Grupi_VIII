99a935930dc3768c8bc6e76f9041191626cbb0a7
Imer-w <im263@bgt.school>
2026-04-16 13:55:17 +0200

    Task4 completed - pointers and memory addresses

--- Day3/task4.c
+++ Day3/task4.c
@@ -1,1 +1,52 @@
+#include <stdio.h>
 
+int main() {
+
+    int a;
+    float b;
+
+    int *ptrA;
+    float *ptrB;
+
+    // input
+    printf("Enter an integer: ");
+    scanf("%d", &a);
+
+    printf("Enter a float: ");
+    scanf("%f", &b);
+
+    // pointer marrin adresat
+    ptrA = &a;
+    ptrB = &b;
+
+    printf("\n--- BEFORE CHANGE ---\n");
+    printf("a value: %d\n", a);
+    printf("a address: %p\n", ptrA);
+    printf("a via pointer: %d\n", *ptrA);
+
+    printf("\nb value: %.2f\n", b);
+    printf("b address: %p\n", ptrB);
+    printf("b via pointer: %.2f\n", *ptrB);
+
+    // ruaj vlerën e vjetër
+    int oldA = a;
+
+    // ndryshim përmes pointer-it
+    *ptrA = *ptrA + 10;
+
+    printf("\n--- AFTER CHANGE ---\n");
+    printf("New a value: %d\n", a);
+
+    // kontroll me if/else
+    if (a > oldA) {
+        printf("Value increased\n");
+    }
+    else if (a < oldA) {
+        printf("Value decreased\n");
+    }
+    else {
+        printf("Value stayed the same\n");
+    }
+
+    return 0;
+}
